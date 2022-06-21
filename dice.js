0488d30999454b95ae19182c533a80252f5ac269
Om Rajput <or6857@srmist.edu.in>
2022-06-21 23:13:10 +0530

    fourth commit

--- dice.js
+++ dice.js
@@ -1,8 +1,37 @@
 
-var randomVariable1 = Math.floor(Math.random() * 6 ) + 1;
+function myFunction(){
 
-var result = randomVariable1 + ".png";
+    var randomVariable1 = Math.floor(Math.random() * 6 ) + 1;
 
-var image1 = document.querySelector(".img1");
+    var result1 = randomVariable1 + ".png";
 
-image1.setAttribute("src", result);
+    var image1 = document.querySelector(".img1");
+
+    image1.setAttribute("src", result1);
+
+
+
+    var randomVariable2 = Math.floor(Math.random() * 6 ) + 1;
+
+    var result2 = randomVariable2 + ".png";
+
+    var image2 = document.querySelector(".img2");
+
+    image2.setAttribute("src", result2);
+
+
+
+    if(randomVariable1>randomVariable2){
+        document.querySelectorAll("h2")[0].innerHTML = "PLAYER 1 WINS !!!"
+    }
+    else if(randomVariable2>randomVariable1)
+    {
+        document.querySelectorAll("h2")[0].innerHTML = "PLAYER 2 WINS !!!"
+    }
+    else{
+        document.querySelectorAll("h2")[0].innerHTML = "ITS A DRAW !!!"
+    }
+}
+
+
+document.getElementById("play").onclick = function() {myFunction()};
